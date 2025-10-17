d919988c3a29a11d6b224bfefb4798a30df33132
Filip <jafilip98@gmail.com>
2025-10-17 18:17:13 +0200

    Feature 1-5

--- src/account.py
+++ src/account.py
@@ -1,5 +1,15 @@
 class Account:
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, pesel, promo_code=None):
         self.first_name = first_name
         self.last_name = last_name
-        self.balance == 0.0
+        self.balance = 0.0
+        self.pesel = pesel
+        if self.is_pesel_valid:
+            self.pesel = pesel
+        else:
+            self.pesel = "invalid"
+
+    def is_pesel_valid(self, pesel):
+        if isinstance(pesel, str) and len(pesel) == 11:
+            return True
+        
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,7 +3,8 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe")
+        account = Account("John", "Doe", "123456789")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0.0
+        
