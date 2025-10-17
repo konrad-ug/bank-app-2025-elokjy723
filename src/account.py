1cb651d3ed5921052f75436ecc36337984b791d4
Filip <jafilip98@gmail.com>
2025-10-17 20:22:48 +0200

    Feature 1-5

--- src/account.py
+++ src/account.py
@@ -4,12 +4,24 @@ class Account:
         self.last_name = last_name
         self.balance = 0.0
         self.pesel = pesel
-        if self.is_pesel_valid:
+        self.promo_code = promo_code
+        print(self.promo_code)
+        if self.is_pesel_valid(pesel):
             self.pesel = pesel
         else:
             self.pesel = "invalid"
+        if self.validate_code(promo_code) and self.validate_year_birth(promo_code) != True:
+            self.balance += 50
 
     def is_pesel_valid(self, pesel):
         if isinstance(pesel, str) and len(pesel) == 11:
             return True
-        
+    def validate_code(self, promo_code):
+        if isinstance(promo_code, str) and promo_code.startswith("PROM_") and len(promo_code) == 8:
+            return True
+    def validate_year_birth(self, promo_code):
+        year_birth = self.pesel[0] + self.pesel[1]
+        stulecie = self.pesel[2] + self.pesel[3]
+        print(year_birth, stulecie)
+        if(int(year_birth) <= 60 and int(stulecie) >= 1 and int(stulecie) <= 12):
+            return True
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,8 +3,41 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
+        account = Account("John", "Doe", "12345678901")
+        assert account.first_name == "John"
+        assert account.last_name == "Doe"
+        assert account.balance == 0.0
+
+    def test_account_creation_pesel_short(self):
         account = Account("John", "Doe", "123456789")
         assert account.first_name == "John"
         assert account.last_name == "Doe"
         assert account.balance == 0.0
-        
+        assert account.pesel == "invalid"
+    def test_account_creation_pesel_long(self):
+        account = Account("John", "Doe", "12345678912314323453425")
+        assert account.first_name == "John"
+        assert account.last_name == "Doe"
+        assert account.balance == 0.0
+        assert account.pesel == "invalid"
+    def test_account_promo_code(self):
+        account = Account("John", "Doe", "12345678901","PROM_XYZ")
+        assert account.balance == 50
+    def test_account_promo_code(self):
+        account = Account("John", "Doe", "12345678901","SPROM_XYZ")
+        assert account.balance == 0
+    def test_account_promo_code(self):
+        account = Account("John", "Doe", "12345678901","PROM_XY")
+        assert account.balance == 0
+    def test_account_promo_code(self):
+        account = Account("John", "Doe", "12345678901","SWER_XY")
+        assert account.balance == 0
+    def test_account_promo_code_year_more(self):
+        account = Account("John", "Doe", "61010100016","PROM_XYZ")
+        assert account.balance == 50
+    def test_account_promo_code_year_less(self):
+        account = Account("John", "Doe", "59010100013","PROM_XYZ")
+        assert account.balance == 0
+    def test_account_promo_code_year_equal(self):
+        account = Account("John", "Doe", "60010100019","PROM_XYZ")
+        assert account.balance == 0
